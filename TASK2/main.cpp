faa271faeb4a887b0c8ec634a6f0313519789d86
Sam4uk <sto.ukr.net@gmail.com>
2021-02-14 23:31:35 +0200

    Оптимізовано швидкість обчислення числа Фібоначі

--- TASK2/main.cpp
+++ TASK2/main.cpp
@@ -3,7 +3,13 @@
 size_t Fibonacci(size_t n) {
   if (n == 0) return 0;
   if (n <= 2) return 1;
-  return Fibonacci(n - 1) + Fibonacci(n - 2);
+  size_t prev_1{1}, prev_2{1}, result{prev_1 + prev_2};
+  for (size_t index{3}; index <= n; index++) {
+    result = prev_1 + prev_2;
+    prev_2 = prev_1;
+    prev_1 = result;
+  }
+  return result;
 }
 
 int main() {
